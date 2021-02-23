c3715df7b9d8bb9e0efc350b9e5a34389a5a4583
Jake Traynor <79319833+SocialZenJake@users.noreply.github.com>
2021-02-23 10:47:01 +0000

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -68,8 +68,8 @@ module.exports = {
     },
     `gatsby-plugin-offline`,
     `gatsby-plugin-gatsby-cloud`,
+    `gatsby-plugin-netlify-cms`
     `gatsby-plugin-netlify`,
-    `gatsby-plugin-netlify-cms`,
     shouldAnalyseBundle && {
       resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
       options: {
